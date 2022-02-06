58ec31a8282046a81142b142750e8d0da8aad9fb
Christopher <chrisriddle17@yahoo.com>
2022-02-06 15:36:17 -0600

    added Unity Input System
    Unity Input System with movement and rotation on character controller with Gamepad and WASD

---
+++ Rat Island/Assets/Scripts/TwinStickMovement.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+public class TwinStickMovement : MonoBehaviour
+{
+    [SerializeField] float _playerSpeed = 5f;
+    [SerializeField] float _gravityValue = -9.81f;
+    [SerializeField] float _controllerDeadzone = 0.1f;
+    [SerializeField] float _gamepadRotateSmoothing = 1000f;
+
+    [SerializeField] bool isGamepad;
+
+    CharacterController _controller;
+
+    Vector2 _movement;
+    Vector2 _aim;
+
+    Vector3 _playerVelocity;
+
+    PlayerControls _playerControls;
+    PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _playerControls = new PlayerControls();
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    void Update()
+    {
+        HandleInput();
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleInput()
+    {
+        _movement = _playerControls.Controls.Movement.ReadValue<Vector2>();
+        _aim = _playerControls.Controls.Aim.ReadValue<Vector2>();
+    }
+
+    void HandleMovement()
+    {
+        Vector3 move = new Vector3(_movement.x, 0, _movement.y);
+        _controller.Move(move * Time.deltaTime * _playerSpeed);
+
+        _playerVelocity.y += _gravityValue * Time.deltaTime;
+        _controller.Move(_playerVelocity * Time.deltaTime);
+    }
+
+    void HandleRotation()
+    {
+        if (isGamepad)
+        {
+            //Rotate our player
+            if (Mathf.Abs(_aim.x) > _controllerDeadzone || Mathf.Abs(_aim.y) > _controllerDeadzone)
+            {
+                Vector3 playerDirection = Vector3.right * _aim.x + Vector3.forward * _aim.y;
+
+                if (playerDirection.sqrMagnitude > 0.0f)
+                {
+                    Quaternion newrotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, newrotation, _gamepadRotateSmoothing * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    public void OnDeviceChange (PlayerInput deviceInput)
+    {
+        isGamepad = deviceInput.currentControlScheme.Equals("Gamepad") ? true : false;
+    }
+}
